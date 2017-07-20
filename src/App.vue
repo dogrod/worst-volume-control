62eb22197104ce3bc048884fde6e9c9067449646
zhulei <zhulei@xkeshi.com>
2017-07-20 10:24:01 +0800

    feat: init project by vue-cli, add ts support

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Inject, Model, Prop, Provide, Vue, Watch } from 'vue-property-decorator'
+
+@Component({
+  name: 'app'
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
